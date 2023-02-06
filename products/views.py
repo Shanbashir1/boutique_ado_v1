b7274c28aec1dde484081fbeac900a216f83b409
Shanbashir1 <shanbashir1@hotmail.com>
2023-02-06 11:26:35 +0000

    Started products template

--- products/views.py
+++ products/views.py
@@ -8,11 +8,13 @@ def all_products(request):
     A view to show all products, including sorting and searching queries
     """
 
-    peoducts = Product.objects.all()
+    products = Product.objects.all()
 
     context = {
         'products' : products,
     }
 
 
+
     return render(request, "products/products.html", context)
+    
