afe226c8bc8f893bff443f379799083c4866a1ac
Shanbashir1 <shanbashir1@hotmail.com>
2023-03-13 11:02:18 +0000

    updated secret keys in settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -180,8 +180,8 @@ if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'boutique-ado4'
     AWS_S3_REGION_NAME = 'eu-west-2'
-    AWS_ACCESS_KEY_ID = os.environ.get('AKIATCQONKGTDU7TDYEK')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('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')
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
